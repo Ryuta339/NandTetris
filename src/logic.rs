b8126e19470776adba360f255e971b1063354dbf
Ryuta339 <soccer.271828.339@gmail.com>
2023-05-25 23:00:09 +0900

    feat: logic operators

--- src/logic.rs
+++ src/logic.rs
@@ -1,12 +1,34 @@
 type Bit = bool;
 
 pub fn nand(a: Bit, b: Bit) -> Bit {
-    ! (a & b)
+    ! (a && b)
+}
+
+pub fn not(a: Bit) -> Bit {
+    nand(a, a)
+}
+
+pub fn and(a: Bit, b: Bit) -> Bit {
+    not(nand(a, b))
+}
+
+pub fn or(a: Bit, b: Bit) -> Bit {
+    nand(not(a), not(b))
+}
+
+pub fn nor(a: Bit, b: Bit) -> Bit {
+    not(or(a, b))
+}
+
+pub fn xor(a: Bit, b: Bit) -> Bit {
+    and(nand(a, b), or(a, b))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    const BOOLS: [Bit; 2] = [false, true];
 
     #[test]
     fn test_nand() {
@@ -14,5 +36,47 @@ mod tests {
         assert_eq!(nand(false, true), true);
         assert_eq!(nand(true, false), true);
         assert_eq!(nand(true, true), false);
+    }
+
+    #[test]
+    fn test_not() {
+        for b in BOOLS.iter() {
+            assert_eq!(not(*b), !*b);
+        }
+    }
+
+    #[test]
+    fn test_and() {
+        for b1 in BOOLS.iter() {
+            for b2 in BOOLS.iter() {
+                assert_eq!(and(*b1, *b2), *b1 && *b2);
+            }
+        }
+    }
+
+    #[test]
+    fn test_or() {
+        for b1 in BOOLS.iter() {
+            for b2 in BOOLS.iter() {
+                assert_eq!(or(*b1, *b2), *b1 || *b2);
+            }
+        }
+    }
+
+    #[test]
+    fn test_nor() {
+        for b1 in BOOLS.iter() {
+            for b2 in BOOLS.iter() {
+                assert_eq!(nor(*b1, *b2), !(*b1 || *b2));
+            }
+        }
+    }
+
+    #[test]
+    fn test_xor() {
+        assert_eq!(xor(false, false), false);
+        assert_eq!(xor(false, true), true);
+        assert_eq!(xor(true, false), true);
+        assert_eq!(xor(true, true), false);
     }
 }
