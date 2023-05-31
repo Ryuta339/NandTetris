7e3c531858245f483a192b14b146a5d74c044694
Ryuta339 <soccer.271828.339@gmail.com>
2023-05-31 21:27:18 +0900

    feat: 16 bits multiplexor

--- src/logic.rs
+++ src/logic.rs
@@ -110,6 +110,28 @@ pub fn or16(a: [Bit; 16], b: [Bit; 16]) -> [Bit; 16] {
     ]
 }
 
+// 16ビットマルチプレクサ
+pub fn mux16(a: [Bit; 16], b: [Bit; 16], sel: Bit) -> [Bit; 16] {
+    [
+        mux(a[0], b[0], sel),
+        mux(a[1], b[1], sel),
+        mux(a[2], b[2], sel),
+        mux(a[3], b[3], sel),
+        mux(a[4], b[4], sel),
+        mux(a[5], b[5], sel),
+        mux(a[6], b[6], sel),
+        mux(a[7], b[7], sel),
+        mux(a[8], b[8], sel),
+        mux(a[9], b[9], sel),
+        mux(a[10], b[10], sel),
+        mux(a[11], b[11], sel),
+        mux(a[12], b[12], sel),
+        mux(a[13], b[13], sel),
+        mux(a[14], b[14], sel),
+        mux(a[15], b[15], sel),
+    ]
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -255,5 +277,21 @@ mod tests {
             true, true, true, false,
             true, true, true, true
         ])
+    }
+
+    #[test]
+    fn test_mux16() {
+        let a: [Bit; 16] = [false; 16];
+        let b: [Bit; 16] = [true; 16];
+        for sel in BOOLS.iter() {
+            let out = mux16(a, b, *sel);
+            for i in 0..16 {
+                if *sel {
+                    assert_eq!(out[i], b[i]);
+                } else {
+                    assert_eq!(out[i], a[i]);
+                }
+            }
+        }
     }
 }
