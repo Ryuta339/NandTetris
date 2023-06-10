48bc140f00ce686e3bb9df18d4747c54af935e5f
Ryuta339 <soccer.271828.339@gmail.com>
2023-06-10 15:25:34 +0900

    feat: add16

--- src/arithmetic.rs
+++ src/arithmetic.rs
@@ -12,9 +12,30 @@ pub fn half_adder(a: Bit, b: Bit) -> [Bit; 2] {
 /// * `a` - first input of a full adder
 /// * `b` - second input of a full adder
 /// * `c` - third input of a full adder
-/// * return - [sum, carry] : [Bit; 3]
+/// * return - [sum, carry] : [Bit; 2]
 pub fn full_adder(a: Bit, b: Bit, c: Bit) -> [Bit; 2] {
-    [xor(xor(a, b), c), or(or(and(a, b), and(b, c)), and(c, a))]
+    [
+        xor(xor(a, b), c), 
+        or(or(and(a, b), and(b, c)), and(c, a))
+    ]
+}
+
+/// 16-bit-width adder
+/// * `a` - first 16-bit-width input of an adder
+/// * `b` - second 16-bit-width input of an adder
+/// * return a + b (mod 0x10000)
+///
+/// Do not detect an overflow.
+pub fn add16(a: [Bit; 16], b: [Bit; 16]) -> [Bit; 16] {
+    let mut carry: Bit = false;
+    let mut sum: [Bit; 16] = [false; 16];
+    a.iter().zip(b.iter()).enumerate()
+        .for_each(|(i, (x, y))| {
+            let tmp = full_adder(*x, *y, carry);
+            sum[i] = tmp[0];
+            carry = tmp[1];
+        });
+    return sum
 }
 
 #[cfg(test)]
@@ -62,5 +83,25 @@ mod tests {
             for i in 0..8 {
                 assert_eq!(full_adder(ins[i][0], ins[i][1], ins[i][2]), expecteds[i]);
             }
+    }
+
+    #[test]
+    fn test_add16() {
+        let arr_a: [u16; 5] = [0, 1, 0, 3, 0x8000];
+        let arr_b: [u16; 5] = [0, 1, 2, 2, 0x8000];
+        for (a, b) in arr_a.iter().zip(arr_b.iter()){
+            // オーバーフローが起きた時は切り捨てる
+            let ans: u16 = a.wrapping_add(*b);
+            let mut bin_a: [Bit; 16] = [false; 16];
+            let mut bin_b: [Bit; 16] = [false; 16];
+            let mut expected: [Bit; 16] = [false; 16];
+            for i in 0..16 {
+                let flag: u16 = 1 << i;
+                bin_a[i] = (a & flag) == flag;
+                bin_b[i] = (b & flag) == flag;
+                expected[i] = (ans & flag) == flag;
+            }
+            assert_eq!(add16(bin_a, bin_b), expected);
+        }
     }
 }
