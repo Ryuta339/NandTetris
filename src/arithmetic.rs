5b2f7e62cb43cc6584df8dc52fa8521991d93383
Ryuta339 <soccer.271828.339@gmail.com>
2023-06-10 15:30:37 +0900

    refactor: to tuple in half_adder

--- src/arithmetic.rs
+++ src/arithmetic.rs
@@ -3,9 +3,9 @@ use crate::logic::*;
 /// Half Adder
 /// * `a` - first input of a half adder
 /// * `b` - second input of a half adder
-/// * return - [sum, carry] : [Bit; 2]
-pub fn half_adder(a: Bit, b: Bit) -> [Bit; 2] {
-    [xor(a, b), and(a, b)]
+/// * return - [sum, carry] : (Bit, Bit)
+pub fn half_adder(a: Bit, b: Bit) -> (Bit, Bit) {
+    ( xor(a, b), and(a, b) )
 }
 
 /// Full Adder
@@ -47,11 +47,11 @@ mod tests {
             ins[i][0] = (i & 1) == 1;
             ins[i][1] = (i & 2) == 2;
         }
-        let expecteds: [[Bit; 2]; 4] = [
-            [false, false],
-            [true, false],
-            [true, false],
-            [false, true],
+        let expecteds: [(Bit, Bit); 4] = [
+            (false, false),
+            (true, false),
+            (true, false),
+            (false, true),
         ];
 
         for i in 0..4 {
